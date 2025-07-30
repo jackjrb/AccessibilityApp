74843caf8f31766693fdba28e9c4093f9ba00c47
Jackeline Brito <jackeline.brito@maistodos.com.br>
2025-07-30 15:59:48 -0300

    create basic input

--- App.tsx
+++ App.tsx
@@ -5,16 +5,19 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useState } from 'react';
+import { StatusBar, StyleSheet, useColorScheme, View, TextInput, TouchableOpacity } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  const [value, onChangeText] = useState('')
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <TextInput style={styles.input} value={value} onChangeText={onChangeText} placeholder='E-mail'/>
+      <TextInput style={styles.input} placeholder='Senha'/>
+      <TouchableOpacity style={styles.button} >Avancar</TouchableOpacity>
     </View>
   );
 }
@@ -22,7 +25,20 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:'center'
   },
+    input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button:{
+    borderRadius:8,
+    borderColor:'#fefefefe',
+    backgroundColor:'#434343',
+    height: 40,
+  }
 });
 
 export default App;
